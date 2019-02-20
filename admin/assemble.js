5729586410eab792b0745a36183757461ac7d00c
Chris Goad <cagoad@gmail.com>
2019-02-20 02:56:19 -0700

    removed some files

--- admin/assemble.js
+++ admin/assemble.js
@@ -27,7 +27,7 @@ const prepend = function (what,arr) {
 }
 
 
-fileLists.core = prepend('core',["root","basic_ops","pageutils","tree","exception","update","instantiate","serialize","deserialize",
+fileLists.core = prepend('core',["root","basic_ops","string_utils","tree","exception","update","instantiate","serialize","deserialize",
                   "install","log"]);
 fileLists.geom = prepend("geom",["geom","geometric_object"]); 
 fileLists.dom = prepend('dom',["environment","data","dom1","jxon","svg","html","domstringify","svg_serialize"]);
